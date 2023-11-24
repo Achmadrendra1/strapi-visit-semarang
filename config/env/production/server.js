973fff583d772247795b195616a3b558991e493f
achmadrendra1 <achmadrendra26@gmail.com>
2023-11-24 08:20:14 +0700

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("HOST_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
